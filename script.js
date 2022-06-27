c75efce396a2f01f9631c82eb68ddedc24d9f4bd
adityapermadi <67409031+adityapermadi@users.noreply.github.com>
2022-06-27 11:35:47 +0700

    comit kedua

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+// show loading
+function loading() {
+    loader.hidden = false;
+    quote.quoteContainer.hidden =true;
+}
+
+// hide loading
+function complete() {
+quoteContainer.hidden = false;
+loader.hidden = true;
+}
+
+
+// show new Quotes
+function newQuote() {
+    
+   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if (!quote.author) {
+        authorText.textContent = 'Unknow';    
+    } else {
+        authorText.textContent = quote.author;
+    }
+    if (quote.text.length > 100) {
+        quoteText.classList.add('long-quote'); 
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+     complete();   
+    quoteText.textContent = quote.text;
+    
+}
+   //    console.log(quote); 
+// get quotes from API
+async function getQuotes() {
+    
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const respone = await fetch(apiUrl);
+        apiQuotes = await respone.json();
+        newQuote();
+    } catch (error) {
+        // catch error here
+    }
+}
+// tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+    // event listners
+    newQuoteBtn.addEventListener('click', newQuote);
+    twitterBtn.addEventListener('click', tweetQuote);
+// on load
+getQuotes();
+newQuote();
